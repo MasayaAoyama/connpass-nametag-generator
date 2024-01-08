46cae6cd5e300b3b9de80c2c7cbcfc431c7dd858
Masaya Aoyama (@amsy810) <amsy810@gmail.com>
2024-01-09 07:56:13 +0900

    tweak

--- connpass_generate_list.sh
+++ connpass_generate_list.sh
@@ -16,11 +16,11 @@ while IFS= read -r line; do
 done <<< "$(cat $FILENAME | cut -d "," -f 2)"
 
 for i in `seq 1 ${#CONNPASS_IDS[@]}`; do
-    curl -s https://connpass.com/user/${CONNPASS_IDS[$i]}/ > /tmp/user_request_cache.html
+    curl -sL https://connpass.com/user/${CONNPASS_IDS[$i]}/ > /tmp/user_request_cache.html
 
     NAME="$(cat /tmp/user_request_cache.html | grep -A1 '<h2 class="title_2">' | tail -n 1 | sed -r 's/^[[:space:]]*|[[:space:]]*$//g')"
     TWITTER_ID="$(cat /tmp/user_request_cache.html | grep http://twitter.com/ | head -n 1 | grep -v connpass_jp | cut -d '"' -f 2 | cut -d "/" -f 4)"
-    IMG_URL="$(cat /tmp/user_request_cache.html | grep '<img src="https://media.connpass.com/thumbs/' | head -n 1  | cut -d '"' -f 2)"
+    IMG_URL="$(cat /tmp/user_request_cache.html | grep '<img src="https://media.connpass.com/thumbs/' | grep 'width="60" height="60"' | head -n 1  | cut -d '"' -f 2)"
 
     echo "$NAME,$TWITTER_ID,$IMG_URL"
 done > $OUTPUT_FILENAME
